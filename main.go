e8cc031b9f6172f3957982bb3929ac6cb80d22f2
DeviNoles <devin8821@gmail.com>
2024-12-31 15:25:41 -0500

    Listen on port

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
 	fmt.Println("Hello, Go!")
+	var uri string = "localhost"
+	var port string = "8080"
+	l, err := net.Listen("tcp", uri+":"+port)
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	fmt.Printf("Listening on %s:%s\n", uri, port)
 }
